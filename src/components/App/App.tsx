4847e939f4be228e7d280711885a7f8d21de5478
Orlov Oleg <Dark.eagle217@yandex.ru>
2023-12-06 12:26:33 +0300

    feat: webpack default

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import classes from './App.module.scss';
+import {Link, Outlet} from "react-router-dom";
+
+export const App = () => {
+
+    return (
+        <div data-testid={'App.DataTestId'}>
+            Root
+        </div>
+    );
+};
+
